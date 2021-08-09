0910cb34f438fc8a09a4de769dcc3feefef506cb
maru8605 <maru8605@hotmail.com>
2021-08-09 12:32:38 -0300

    modal

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,6 +1,45 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { ModalContext } from '../context/ModalContext'
+
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 600,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 const Recipes = ({recipe}) => {
+
+    // configuracion Modal
+    const [styleModal] = useState(getModalStyle)
+    const [open, setOpen] = useState(false)
+
+    const classes = useStyles()
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () =>{
+        setOpen(false)
+    }
     
     const {saveIdRecipe} = useContext(ModalContext)
 
@@ -13,11 +52,25 @@ const Recipes = ({recipe}) => {
                 <div className='card-body'>
                     <button
                         type='button'
+
                         className='btn btn-block button'
                         onClick={ () =>{
                             saveIdRecipe(recipe.idDrink)
+                            handleOpen()
                         }}
+                        
                     >Ver receta</button>
+
+                    <Modal
+                     open={open}
+                     onClose={ () =>{
+                         handleClose()
+                         saveIdRecipe(null)
+                     }}>
+                        <div style={styleModal} className={classes.paper}>
+                            <h1>desde modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,10 +1,27 @@
 import { createContext, useState, useEffect } from "react";
+import axios from 'axios'
 
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
     
     const [idrecipe, saveIdRecipe] = useState(null)
+    const [ recipe, saveRecipe] = useState({})
+
+    useEffect(() => {
+        
+        const getRecipe = async () => {
+           
+            if(!idrecipe) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idrecipe}`;
+
+            const resultado = await axios.get(url);
+
+           saveRecipe(resultado.data.drinks[0]);
+        }
+        getRecipe()
+    }, [idrecipe])
 
     return(
         <ModalContext.Provider
